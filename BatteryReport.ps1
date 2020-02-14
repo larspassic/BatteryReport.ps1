f72578e19ddc7b4a2647fcf9b1e6d668260c2b27
Lars Passic <43658463+larspassic@users.noreply.github.com>
2020-02-13 17:44:26 -0800

    Revert "Revert "initial commit""
    This reverts commit 3adcc203017bc280f3eb6b5a86b369e8f853b15e.

---
+++ BatteryReport.ps1
@@ -0,0 +1,22 @@
+﻿#Lock in start time
+$start = Get-Date
+$filedate = Get-Date -Format FileDateTime
+$reportname = "Battery Report $filedate"
+
+#Log start time to the report file
+Write-Host "Starting battery life test at $start"
+"Starting battery life test at $start" | Out-File "$reportname.txt"
+
+#Begin the loop
+Write-Host "Waiting for battery to drain. This will automatically close when the battery reaches 6%..."
+While ((Get-WmiObject win32_battery).estimatedChargeRemaining -gt 6){Sleep 60}
+
+#Record the end time
+$end=Get-Date
+Write-Host "Ending battery life test at $end"
+"Ending battery life test at $end" | Out-File "$reportname.txt" -Append
+$result=($end - $start).TotalSeconds
+
+#Log the result to the report file
+Write-Host "Battery test duration: $result seconds"
+"Battery test duration: $result" | Out-File "$reportname.txt" -Append
